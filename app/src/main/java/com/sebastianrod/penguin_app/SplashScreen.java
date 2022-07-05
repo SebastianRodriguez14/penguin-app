10d5b5cd8926a32659831b9fb5cf8b124524b814
SebastianRodriguez14 <sebastianrodriguezventura@gmail.com>
2022-07-05 18:41:37 -0500

    add splash screen

---
+++ app/src/main/java/com/sebastianrod/penguin_app/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.sebastianrod.penguin_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceView){
+        super.onCreate(savedInstanceView);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 1500);
+    }
+
+
+}
